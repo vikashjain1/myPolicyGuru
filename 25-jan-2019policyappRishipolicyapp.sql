e0cc626b543de86c28671e4b20e5a4d0c7892a6e
Rishi <test@test.com>
2019-01-25 18:33:22 +0530

    left menu and update profile of agent with new db changes

--- 25-jan-2019policyappRishipolicyapp.sql
+++ 25-jan-2019policyappRishipolicyapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2019 at 01:28 PM
+-- Generation Time: Jan 25, 2019 at 02:02 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.0.15
 
@@ -118,13 +118,7 @@ CREATE TABLE `communities` (
 --
 
 INSERT INTO `communities` (`id`, `user_id`, `subject`, `details`, `status`, `created`, `modified`) VALUES
-(1, 1, 'test subject', 'details 1', 0, NULL, NULL),
-(2, 1, 'test subject', 'details 2', 0, NULL, NULL),
-(3, 1, 'test subject', 'b            ', 0, NULL, NULL),
-(4, 1, 'subject 1', 'details 4', 0, '2018-12-28 06:48:59', '2018-12-28 06:48:59'),
-(5, 2, 'subject 2', 'details 5', 0, '2018-12-28 06:48:59', '2018-12-28 06:48:59'),
-(6, 1, 'goo d by', 'goo d bygoo d bygoo d bygoo d by', 1, '2019-01-17 12:46:21', '2019-01-17 12:46:21'),
-(7, 3, 'hua bhy raj', 'hua bhy raj', 1, '2019-01-19 09:08:28', '2019-01-19 09:08:28');
+(8, 1, 'Good', 'Good one', 1, '2019-01-25 07:49:11', '2019-01-25 07:49:11');
 
 -- --------------------------------------------------------
 
@@ -146,9 +140,7 @@ CREATE TABLE `communities_likes` (
 --
 
 INSERT INTO `communities_likes` (`id`, `community_id`, `user_id`, `status`, `created`, `modified`) VALUES
-(3, 1, 1, 1, '2019-01-18 14:50:43', '2019-01-18 15:42:11'),
-(7, 6, 1, 1, '2019-01-19 09:02:33', '2019-01-19 09:02:33'),
-(8, 6, 3, 1, '2019-01-19 09:04:46', '2019-01-19 09:04:46');
+(9, 8, 1, 0, '2019-01-25 07:49:32', '2019-01-25 08:05:56');
 
 -- --------------------------------------------------------
 
@@ -171,9 +163,7 @@ CREATE TABLE `communities_responses` (
 --
 
 INSERT INTO `communities_responses` (`id`, `community_id`, `user_id`, `response`, `status`, `created`, `modified`) VALUES
-(1, 1, 1, 'good', 1, '2019-01-17 12:33:12', '2019-01-17 12:33:12'),
-(2, 6, 3, 'good nahi very bad raj user', 1, '2019-01-19 09:07:27', '2019-01-19 09:07:27'),
-(3, 1, 3, 'gabru raj', 1, '2019-01-19 09:07:54', '2019-01-19 09:07:54');
+(4, 8, 1, 'Post response', 1, '2019-01-25 07:49:22', '2019-01-25 07:49:22');
 
 -- --------------------------------------------------------
 
@@ -241,6 +231,7 @@ INSERT INTO `policy_types` (`id`, `policy_name`) VALUES
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `user_type_code` varchar(50) DEFAULT 'NORMAL',
   `name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `account_type` varchar(255) NOT NULL,
@@ -268,15 +259,15 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `account_type`, `address`, `city`, `state`, `zip`, `how_many_homes`, `how_many_cars`, `boat_exists`, `how_many_children`, `small_business_exists`, `business_name`, `married`, `phone`, `password`, `status`, `request_date`, `created`, `modified`, `user_type_id`) VALUES
-(1, 'Rishi Kapoor', 'rishi@test.com', 'business', 'sect4012', 'lko1', 'hary', '122001', 2, 2, 0, 2, 0, 'japu', 0, '', '$2y$10$xjzItDbKNjt4GnarNt/n5OD1lDiZXvIZJG9oJ/0zq6Oer.HZzhyf2', 0, NULL, '0000-00-00 00:00:00', '2018-12-28 06:32:38', 1),
-(2, 'Vikash', 'vikajjjsh@test.com', 'individual', 'sect40', 'lko', 'hary', '122001', 2, 2, 1, 2, 1, 'japu', 1, '', '$2y$10$JaVaMUnNRX60rejeVr4K1ON5ZyRhTq5uMBcLcYnkzHkYvdhH7SO8O', 0, NULL, '0000-00-00 00:00:00', '2019-01-01 10:09:16', 1),
-(3, 'raj kumar', 'raj@test.com', 'business', 'sect40', 'lko', 'hary', '122001', 2, 2, 1, 2, 1, 'japu new', 0, '', '$2y$10$TxR/S/4XOvTPuSglCaI72enw7ZWBVuhtGW9A3i8mv9cpDKZ6hNpsq', 0, NULL, '0000-00-00 00:00:00', '2019-01-22 12:59:47', 2),
-(5, '', 'rish1i@test.com', '', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$UU44uaPsc7PG/QwSrfYdJO1uKomgtuNXkRK7BjiwpSsMgzTQiSmwq', 0, NULL, '0000-00-00 00:00:00', '2019-01-06 13:59:07', 1),
-(6, '', 'vikash@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$vnSb7f6Ya59Kn4cYA.tjPOyqR9YlwNzAAXJ1vSS/skV6MYXlvJXHS', 0, NULL, '2019-01-24 11:36:10', '2019-01-24 11:36:10', 1),
-(7, '', 'rishi@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$LFQhpXBvJ7FxvB4juISNJuaGdETvwBOmeCCkCiffvw3vckoQ0y7E.', 0, NULL, '2019-01-24 11:40:29', '2019-01-24 11:57:27', 2),
-(10, '', 'rohit@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$HIyyaZcZNdhl1UC4kuRk1OKCf0pARBAOrWcGZnvyfw8zaf5dTaN9q', 0, NULL, '2019-01-24 11:59:50', '2019-01-24 11:59:50', 2),
-(11, '', 'jaidev@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$Wp8vJ6SyB17hzxVKE/DYceozT0yQ8722Fa/cdcbqzc1SC5fjwqTD6', 0, NULL, '2019-01-24 12:01:31', '2019-01-24 12:01:31', 2);
+INSERT INTO `users` (`id`, `user_type_code`, `name`, `email`, `account_type`, `address`, `city`, `state`, `zip`, `how_many_homes`, `how_many_cars`, `boat_exists`, `how_many_children`, `small_business_exists`, `business_name`, `married`, `phone`, `password`, `status`, `request_date`, `created`, `modified`, `user_type_id`) VALUES
+(1, 'NORMAL', 'Rishi Kapoor', 'rishi@test.com', 'business', 'sect4012', 'lko1', 'hary', '122001', 2, 2, 0, 2, 0, 'japu', 0, '', '$2y$10$xjzItDbKNjt4GnarNt/n5OD1lDiZXvIZJG9oJ/0zq6Oer.HZzhyf2', 0, NULL, '0000-00-00 00:00:00', '2018-12-28 06:32:38', 1),
+(2, 'NORMAL', 'Vikash', 'vikajjjsh@test.com', 'individual', 'sect40', 'lko', 'hary', '122001', 2, 2, 1, 2, 1, 'japu', 1, '', '$2y$10$JaVaMUnNRX60rejeVr4K1ON5ZyRhTq5uMBcLcYnkzHkYvdhH7SO8O', 0, NULL, '0000-00-00 00:00:00', '2019-01-01 10:09:16', 1),
+(3, 'AGENT', 'raj kumar', 'raj@test.com', 'business', 'sect40', 'lko', 'hary', '122001', 2, 2, 1, 2, 1, 'japu new', 0, '', '$2y$10$TxR/S/4XOvTPuSglCaI72enw7ZWBVuhtGW9A3i8mv9cpDKZ6hNpsq', 0, NULL, '0000-00-00 00:00:00', '2019-01-25 12:35:31', 2),
+(5, 'NORMAL', '', 'rish1i@test.com', '', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$UU44uaPsc7PG/QwSrfYdJO1uKomgtuNXkRK7BjiwpSsMgzTQiSmwq', 0, NULL, '0000-00-00 00:00:00', '2019-01-06 13:59:07', 1),
+(6, 'NORMAL', '', 'vikash@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$vnSb7f6Ya59Kn4cYA.tjPOyqR9YlwNzAAXJ1vSS/skV6MYXlvJXHS', 0, NULL, '2019-01-24 11:36:10', '2019-01-24 11:36:10', 1),
+(7, 'AGENT', '', 'rishi@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$LFQhpXBvJ7FxvB4juISNJuaGdETvwBOmeCCkCiffvw3vckoQ0y7E.', 0, NULL, '2019-01-24 11:40:29', '2019-01-25 12:35:13', 2),
+(10, 'AGENT', 'rohit yadav', 'rohit@agent.com', 'individual', 'sect40', 'lko', 'hary', '122001', 3, 3, 0, 3, 0, 'japu', 0, '', '$2y$10$HIyyaZcZNdhl1UC4kuRk1OKCf0pARBAOrWcGZnvyfw8zaf5dTaN9q', 0, NULL, '2019-01-24 11:59:50', '2019-01-25 12:35:00', 2),
+(11, 'AGENT', '', 'jaidev@agent.com', 'agent', '', '', '', '', 0, 0, 0, 0, 0, '', 0, '', '$2y$10$Wp8vJ6SyB17hzxVKE/DYceozT0yQ8722Fa/cdcbqzc1SC5fjwqTD6', 0, NULL, '2019-01-24 12:01:31', '2019-01-25 12:34:50', 2);
 
 -- --------------------------------------------------------
 
@@ -387,17 +378,17 @@ ALTER TABLE `claim_types`
 -- AUTO_INCREMENT for table `communities`
 --
 ALTER TABLE `communities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `communities_likes`
 --
 ALTER TABLE `communities_likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `communities_responses`
 --
 ALTER TABLE `communities_responses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `policies`
 --
