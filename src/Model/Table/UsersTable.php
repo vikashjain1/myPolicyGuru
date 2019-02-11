b22df4d469a2a390bfc7519111a6d68ec5723f53
vikashjain1 <46296571+vikashjain1@users.noreply.github.com>
2019-02-11 16:33:17 +0530

    Add files via upload

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -16,31 +16,79 @@ class UsersTable extends Table
 						 $this->belongsTo('UserType');
 						 $this->hasMany('AgentsUsers');
 
+    }	
+	
+	public function validationAdd(Validator $validator)
+    {
+        $validator
+			->email('email')
+			->requirePresence('email')
+			->notEmpty('email', 'An email address is required.');
+			//->notBlank('email', 'An email address is required.');
+
+		$validator
+			->requirePresence('password')
+			->notEmpty('password', 'A password is required.')
+			->add('password', 'minlength',['rule' => ['minlength', 5]]);
+
+		$validator
+			->requirePresence('cnfpassword')
+			->notEmpty('cnfpassword', 'A Confirm Password is required.')
+			->add('cnfpassword', 'minlength',['rule' => ['minlength', 5]])
+			->sameAs('cnfpassword','password','Passwords and Confirm Password are not equal.');
+		
+		return $validator;
     }
 	
-	public function validationDefault(Validator $validator){
-	 $validator->notEmpty('email','Please, enter your email !')->add('email', [
-					'email' => [
-						'rule' => ['email'],
-						'message'=>" Please, enter a valid email!"
-						]
-					])
-			->notEmpty('password','Please, enter your password !')
-					->add('password', [
-					'compare' => [
-						'rule' => ['compareWith', 'cnfpassword'],
-						'message'=>"Password mismatch password confirm !"
-						]
-					])
-					
-					;
-					return $validator;																		
+	public function validationEdit (Validator $validator){
+		$validator->requirePresence('first_name')
+			->notEmpty('first_name','First Name cannot be left empty.')
+			->notEmpty('last_name','Last Name cannot be left empty.')
+			->notEmpty('address','Address cannot be left empty.')
+			->notEmpty('city','City cannot be left empty.')
+			->notEmpty('state','State cannot be left empty.')
+			->notEmpty('zip','Zip cannot be left empty.');
+		return $validator;																		
 	}
+	
+	public function validationEditagent (Validator $validator){
+		$validator->requirePresence('first_name')
+			->notEmpty('first_name','First Name cannot be left empty.')
+			->notEmpty('last_name','Last Name cannot be left empty.')
+			->notEmpty('address','Address cannot be left empty.')
+			->notEmpty('city','City cannot be left empty.')
+			->notEmpty('state','State cannot be left empty.')
+			->notEmpty('zip','Zip cannot be left empty.');
+			
+		return $validator;																		
+	}
+	
+	public function validationChangepwd(Validator $validator)
+    {
+		$validator
+			->requirePresence('oldpassword')
+			->notEmpty('oldpassword', 'Old Password cannot be left blank.')
+			->notBlank('email', 'An email address is required.')
+			->add('oldpassword', 'minlength',['rule' => ['minlength', 5]]);
+
+		$validator
+			->requirePresence('newpwd')
+			->notEmpty('newpwd', 'New Password cannot be left blank.')
+			->add('newpwd', 'minlength',['rule' => ['minlength', 5]]);
+
+		$validator
+			->requirePresence('cnfpassword')
+			->notEmpty('cnfpassword', 'Confirm Password cannot be left blank.')
+			->add('cnfpassword', 'minlength',['rule' => ['minlength', 5]])
+			->sameAs('cnfpassword','newpwd','New Passwords and Confirm Password are not equal.');
+		
+		return $validator;
+    }
+	
 	/**
 	* Returns a rules checker object . It is used  for validating data
 	* I used it her to check if email is unique 
 	**/
-	
 	public function buildRules(RulesChecker $rules)
     {
         $rules->add($rules->isUnique(['email']));
